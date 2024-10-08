247fb52c8f278a3b9b419524a05125a31c585675
jongbin26 <010jjj@naver.com>
2024-10-08 12:39:59 +0900

    chore: 프로젝트 초기 세팅

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,20 @@
-import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier'; // prettier 설정 추가
 
-export default tseslint.config(
-	{ ignores: ['dist'] },
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+	{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	prettier, // prettier 추가
 	{
-		extends: ['plugin:prettier/recommended'],
-		files: ['**/*.{ts,tsx}'],
-		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser,
-		},
-		plugins: {
-			'react-hooks': reactHooks,
-			'react-refresh': reactRefresh,
-		},
 		rules: {
-			...reactHooks.configs.recommended.rules,
-			'react-refresh/only-export-components': [
-				'warn',
-				{ allowConstantExport: true },
-			],
+			'react/react-in-jsx-scope': 'off',
 		},
 	},
-);
+];
