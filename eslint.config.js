c19f04ad023c1441d8f41e8b2b7aa6e8ae20de0a
jongbin26 <010jjj@naver.com>
2024-10-08 10:57:21 +0900

    chore: 프로젝트 초기 세팅

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {ignores: ['dist']},
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: ['airbnb-base', 'plugin:prettier/recommended'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -21,8 +21,8 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        {allowConstantExport: true},
       ],
     },
   },
-)
+);
