1ba2b98f717af09c61418708e27a8229276be78a
최동준 <choedongjun@choedongjun-ui-MacBookAir.local>
2024-11-14 12:28:03 +0900

    feat: splash 페이지 개발

--- src/pages/SplashPage/SplashPage.tsx
+++ src/pages/SplashPage/SplashPage.tsx
@@ -1,4 +1,60 @@
+import { S } from './style';
+import TreeIcon from '../../lib/assets/images/splash_tree_icon.png';
+import Text from '../../components/common/Text';
+import { useEffect, useState } from 'react';
+import { calculateTimeUntilChristmas } from '../../utils/time';
+import Button from '../../components/common/Button';
+import { useNavigate } from 'react-router-dom';
+
 const SplashPage = () => {
-  return <>SplashPage</>;
+  const [timeLeft, setTimeLeft] = useState(calculateTimeUntilChristmas());
+  const navigate = useNavigate();
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeUntilChristmas());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+  return (
+    <S.SplashPageContainer className="layout-padding">
+      <S.TitleWrapper>
+        <img src={TreeIcon} alt="나무 아이콘" />
+        <S.FlexBox>
+          <Text typograph={'headlineMedium'} color={'Yellow'}>
+            크리스마까지 남은 기간
+          </Text>
+          <Text
+            typograph={'headlineMedium'}
+            color={'Yellow'}
+            style={{ fontWeight: 700 }}
+          >
+            {`단 ${timeLeft.days.toString()}일!`}
+          </Text>
+        </S.FlexBox>
+      </S.TitleWrapper>
+      <S.TimeText>
+        {String(timeLeft.hours + timeLeft.days * 24).padStart(2, '0')}:
+        {String(timeLeft.minutes).padStart(2, '0')}:
+        {String(timeLeft.seconds).padStart(2, '0')}
+      </S.TimeText>
+      <S.FlexBox>
+        <Text typograph={'titleMedium'} color={'Yellow'}>
+          시즌 평균 신청 수 0,000건
+        </Text>{' '}
+        <Text typograph={'titleMedium'} color={'Yellow'}>
+          성비 남0 : 여0, 매칭률 00%
+        </Text>
+      </S.FlexBox>
+      <Button
+        buttonType={'yellow'}
+        onClick={() => {
+          navigate('/start');
+        }}
+      >
+        눈 맞을 짝을 찾아 떠나볼까요?
+      </Button>
+    </S.SplashPageContainer>
+  );
 };
 export default SplashPage;
