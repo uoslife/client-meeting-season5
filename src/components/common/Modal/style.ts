d1df22bc7aa71e6704b71126f79b38a1c2eadc6f
jongbin26 <010jjj@naver.com>
2024-11-14 13:19:18 +0900

    style: 모달 로직과 스타일 분리

---
+++ src/components/common/Modal/style.ts
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components';
+import { COLORS } from '../../../lib/constants';
+import { ModalWrapperPropsType } from './Modal';
+
+const S = {
+  Wrapper: styled.div<ModalWrapperPropsType>`
+    z-index: 3;
+    opacity: 0;
+    width: 100%;
+    margin: 20px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+
+    background-color: ${COLORS.White};
+    padding: 20px;
+    border-radius: 12px;
+    pointer-events: none;
+
+    ${(props) =>
+      props.isOpen &&
+      css`
+        opacity: 1;
+        pointer-events: auto;
+      `}
+  `,
+};
+
+export default S;
