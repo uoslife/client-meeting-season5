e749c464eadb46ba2d2f0c050c769706bf63634e
최동준 <choedongjun@choedongjun-ui-MacBookAir.local>
2024-11-14 12:28:03 +0900

    feat: splash 페이지 개발

---
+++ src/utils/time.ts
@@ -0,0 +1,20 @@
+export const calculateTimeUntilChristmas = () => {
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const christmas = new Date(currentYear, 11, 25);
+
+  if (now > christmas) {
+    christmas.setFullYear(currentYear + 1);
+  }
+
+  const timeDiff = christmas.getTime() - now.getTime();
+
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+  return { days, hours, minutes, seconds };
+};
