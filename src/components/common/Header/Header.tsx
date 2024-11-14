a2dab82731d0706515411c60746ad96c8683b813
jongbin26 <010jjj@naver.com>
2024-11-15 00:36:30 +0900

    feat: Header 공동 컴포넌트 구현

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,79 @@
+import Text from '../Text';
+import arrowBack from '../../../lib/assets/icon/arrow-back.svg';
+import logout from '../../../lib/assets/icon/logout.svg';
+import close from '../../../lib/assets/icon/close.svg';
+import more from '../../../lib/assets/icon/more_horizon.svg';
+import S from './style';
+
+type rightButtonType = 'close' | 'logout' | 'more' | 'none';
+
+interface HeaderPropsType {
+  title: string;
+  isGoBackButton?: boolean;
+  rightButtonType?: rightButtonType;
+  leftButtonCallback?: () => void;
+  rightButtonCallback?: () => void;
+}
+
+const Header = ({
+  title,
+  isGoBackButton = true,
+  rightButtonType = 'none',
+  leftButtonCallback,
+  rightButtonCallback,
+}: HeaderPropsType) => {
+  const rightButtonIcon = (rightButtonType: rightButtonType) => {
+    switch (rightButtonType) {
+      case 'close':
+        return close;
+      case 'logout':
+        return logout;
+      case 'more':
+        return more;
+    }
+  };
+  const iconLocationAttribute = () => {
+    if (isGoBackButton && rightButtonType !== 'none') return 'space-between';
+    if (!isGoBackButton && rightButtonType !== 'none') return 'end';
+    if (isGoBackButton && rightButtonType === 'none') return 'start';
+    return 'start';
+  };
+  return (
+    <S.Wrapper justifyContent={iconLocationAttribute()}>
+      {isGoBackButton && (
+        <img
+          src={arrowBack}
+          alt="뒤로가기"
+          width={20}
+          height={20}
+          style={{ zIndex: 1 }}
+          onClick={leftButtonCallback && leftButtonCallback}
+        />
+      )}
+
+      <Text
+        color={'Blue70'}
+        typograph={'titleMedium'}
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+        }}
+      >
+        {title}
+      </Text>
+      {rightButtonType !== 'none' && (
+        <img
+          src={rightButtonIcon(rightButtonType)}
+          alt="오른쪽버튼"
+          width={20}
+          height={20}
+          style={{ zIndex: 1 }}
+          onClick={rightButtonCallback && rightButtonCallback}
+        />
+      )}
+    </S.Wrapper>
+  );
+};
+export default Header;
