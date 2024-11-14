128103ba8c1fe60abb8be7e9a0caafadd30490d5
jongbin26 <010jjj@naver.com>
2024-11-14 11:55:05 +0900

    feat: Modal 공동 컴포넌트 구현

---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import BlackScreen from '../components/common/BlackScreen';
+import styled from 'styled-components';
+import Modal from '../components/common/Modal';
+
+export interface UseModalPropsType {
+  title: string;
+  description?: string;
+  isSideButton: boolean;
+  mainButtonText?: string;
+  sideButtonText?: string;
+  mainButtonCallback?: () => void;
+}
+
+const useModal = ({
+  title,
+  description,
+  isSideButton,
+  mainButtonText = '확인',
+  sideButtonText = '취소',
+  mainButtonCallback = () => {},
+}: UseModalPropsType) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isPending, setIsPending] = useState<boolean>(false);
+
+  const open = () => setIsOpen(true);
+  const close = () => {
+    setIsPending(false);
+    setIsOpen(false);
+  };
+
+  const render = ({ children = null } = {}) => {
+    const handleButtonClick = () => {
+      if (isPending) {
+        return;
+      }
+      setIsPending(true);
+      mainButtonCallback();
+      close();
+    };
+
+    const modalProps = {
+      isOpen,
+      title,
+      description,
+      sideButtonText,
+      mainButtonText,
+      handleButtonClick,
+      isSideButton,
+      close,
+    };
+
+    return (
+      <S.Wrapper isOpen={isOpen}>
+        <BlackScreen isOpen={isOpen} onClick={close} />
+        <Modal {...modalProps}>{children}</Modal>
+      </S.Wrapper>
+    );
+  };
+
+  return { open, close, render };
+};
+export default useModal;
+
+type ModalWrapperPropsType = {
+  isOpen: boolean;
+};
+
+const S = {
+  Wrapper: styled.div<ModalWrapperPropsType>`
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 50vh;
+    left: 50vw;
+    transform: translate(-50%, -50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 200;
+    ${(props) => !props.isOpen && 'opacity:0;pointer-events: none;'}
+  `,
+};
