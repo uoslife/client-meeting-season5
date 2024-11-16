ff189f8a75146d6304ba739536e079a7c97feb31
jongbin26 <010jjj@naver.com>
2024-11-17 00:27:31 +0900

    style: SecondChocePage 스타일 분리

--- src/step/BasicProfile/Second/SecondChoicePage.tsx
+++ src/step/BasicProfile/Second/SecondChoicePage.tsx
@@ -3,10 +3,13 @@ import Indicator from '../../../components/common/Indicator';
 import Text from '../../../components/common/Text';
 import Radio from '../../../components/common/Radio';
 import Button from '../../../components/common/Button';
+import { useEffect } from 'react';
 
 interface SecondChoicePage {
   academicStatus: '학부생' | '대학원생' | '졸업생' | undefined;
-  setAcademicStatus: (academicStatus: '학부생' | '대학원생' | '졸업생') => void;
+  setAcademicStatus: (
+    academicStatus: '학부생' | '대학원생' | '졸업생' | undefined,
+  ) => void;
   handleNextButtonClick: () => void;
 }
 
@@ -15,75 +18,61 @@ const SecondChoicePage = ({
   setAcademicStatus,
   handleNextButtonClick,
 }: SecondChoicePage) => {
+  useEffect(() => {
+    setAcademicStatus(undefined);
+  }, []);
   return (
-    <S.Wrapper>
-      <div
-        className="layout-padding"
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100vh',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div
+    <S.Wrapper className="layout-padding">
+      <S.TitleWrapper>
+        <Indicator depth={3} currentLevel={2} />
+        <S.TextWrapper>
+          <Text color={'Blue90'} typograph={'headlineMedium'}>
+            신분을 선택해주세요
+          </Text>
+        </S.TextWrapper>
+        <form
           style={{
             display: 'flex',
-            width: '100%',
             flexDirection: 'column',
-            paddingTop: 20,
+            gap: 8,
+            paddingTop: 40,
           }}
         >
-          <Indicator depth={3} currentLevel={1} />
-          <S.TextWrapper>
-            <Text color={'Blue90'} typograph={'headlineMedium'}>
-              신분을 선택해주세요
-            </Text>
-          </S.TextWrapper>
-          <form
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: 8,
-              paddingTop: 40,
+          <Radio
+            label={'학부생'}
+            name="academic-status"
+            value="학부생"
+            onClick={() => {
+              setAcademicStatus('학부생');
             }}
-          >
-            <Radio
-              label={'학부생'}
-              name="s"
-              value="학부생"
-              onClick={() => {
-                setAcademicStatus('학부생');
-              }}
-            />
-            <Radio
-              label={'졸업생'}
-              name="s"
-              value="졸업생"
-              onClick={() => {
-                setAcademicStatus('졸업생');
-              }}
-            />
-            <Radio
-              label={'대학원생'}
-              name="s"
-              value="대학원생"
-              onClick={() => {
-                setAcademicStatus('대학원생');
-              }}
-            />
-          </form>
-        </div>
-        <div style={{ paddingBottom: 36 }}>
-          <Button
-            type="submit"
-            buttonColor={'primary'}
-            disabled={!academicStatus}
-            onClick={handleNextButtonClick}
-          >
-            다음
-          </Button>
-        </div>
+          />
+          <Radio
+            label={'졸업생'}
+            name="academic-status"
+            value="졸업생"
+            onClick={() => {
+              setAcademicStatus('졸업생');
+            }}
+          />
+          <Radio
+            label={'대학원생'}
+            name="academic-status"
+            value="대학원생"
+            onClick={() => {
+              setAcademicStatus('대학원생');
+            }}
+          />
+        </form>
+      </S.TitleWrapper>
+      <div style={{ paddingBottom: 36 }}>
+        <Button
+          type="submit"
+          buttonColor={'primary'}
+          disabled={!academicStatus}
+          onClick={handleNextButtonClick}
+        >
+          다음
+        </Button>
       </div>
     </S.Wrapper>
   );
@@ -94,6 +83,22 @@ export default SecondChoicePage;
 const S = {
   Wrapper: styled.div`
     width: 100%;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: space-between;
+  `,
+  TitleWrapper: styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    padding-top: 20px;
+  `,
+  Form: styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding-top: 40;
   `,
   TextWrapper: styled.div`
     display: flex;
