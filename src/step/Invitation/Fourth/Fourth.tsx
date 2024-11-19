83c89608a313e881639937da7c5c94d4c0d14dfe
jongbin26 <010jjj@naver.com>
2024-11-20 03:16:21 +0900

    feat: 팅장 권한만 Fourth Step GroupDetailPage 라우팅 추가

--- src/step/Invitation/Fourth/Fourth.tsx
+++ src/step/Invitation/Fourth/Fourth.tsx
@@ -1,17 +1,27 @@
 import S from './style';
 import Button from '../../../components/common/Button';
 import { FourthType } from '../../../pages/InvitationPage/InvitationPage';
+import { useNavigate } from 'react-router-dom';
 
 const Fourth = (props: { context: FourthType }) => {
+  const navigate = useNavigate();
   return (
     <S.FormContainer className="layout-padding">
       <S.MainContainer></S.MainContainer>
 
-      <S.ButtonWrapper>
-        <Button buttonColor="primary" type="submit" onClick={() => {}}>
-          다음
-        </Button>
-      </S.ButtonWrapper>
+      {props.context.isTeamLeader && (
+        <S.ButtonWrapper>
+          <Button
+            buttonColor="primary"
+            type="submit"
+            onClick={() => {
+              navigate('/auth/detail/group');
+            }}
+          >
+            다음
+          </Button>
+        </S.ButtonWrapper>
+      )}
     </S.FormContainer>
   );
 };
