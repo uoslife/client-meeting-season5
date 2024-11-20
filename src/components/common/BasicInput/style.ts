afe258c782474418683a9d4a5cbf1bef9adcb762
최동준 <choedongjun@choedongjun-ui-MacBookAir.local>
2024-11-21 05:27:04 +0900

    style: BasicInput placeholder 스타일 수정

--- src/components/common/BasicInput/style.ts
+++ src/components/common/BasicInput/style.ts
@@ -3,16 +3,30 @@ import { COLORS } from '../../../lib/constants';
 
 export const S = {
   Input: styled.input`
-    padding: 12px 0px;
+    padding: 12px 0;
 
-    border: 0px;
+    border: 0;
     border-bottom: 1px solid ${COLORS.Blue20};
 
     width: 100%;
     height: 100%;
 
     font-size: 1.6rem;
-    font-weight: 500,
-    line-height: 2.4rem
+    font-weight: 500;
+    line-height: 2.4rem;
+
+    color: ${COLORS.Blue90}
+
+    ::-webkit-input-placeholder {
+      color: ${COLORS.Blue20};
+    }
+
+    ::-ms-input-placeholder {
+      color: ${COLORS.Blue20};
+    }
+
+    ::placeholder {
+      color: ${COLORS.Blue20};
+    }
   `,
 };
