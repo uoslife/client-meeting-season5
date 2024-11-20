d6b752a21373dc655bff144458bc2314ba91817b
최동준 <choedongjun@choedongjun-ui-MacBookAir.local>
2024-11-21 01:58:09 +0900

    feat: Invite step 4 개발

--- src/step/Invitation/Fourth/Fourth.tsx
+++ src/step/Invitation/Fourth/Fourth.tsx
@@ -2,12 +2,44 @@ import S from './style';
 import Button from '../../../components/common/Button';
 import { FourthType } from '../../../pages/InvitationPage/InvitationPage';
 import { useNavigate } from 'react-router-dom';
+import Text from '../../../components/common/Text';
 
 const Fourth = (props: { context: FourthType }) => {
   const navigate = useNavigate();
   return (
     <S.FormContainer className="layout-padding">
-      <S.MainContainer></S.MainContainer>
+      <S.MainContainer>
+        <S.EntryContainer>
+          <Text typograph={'headlineMedium'} color={'Blue90'}>
+            팅 결성 완료!
+          </Text>
+          <S.EntryList>
+            <S.EntryItem>
+              <Text typograph={'bodyLargeMedium'} color={'Blue90'}>
+                우채윤
+              </Text>
+            </S.EntryItem>
+            <S.EntryItem>
+              <Text typograph={'bodyLargeMedium'} color={'Blue90'}>
+                우채윤
+              </Text>
+            </S.EntryItem>
+            <S.EntryItem>
+              <Text typograph={'bodyLargeMedium'} color={'Blue90'}>
+                우채윤
+              </Text>
+            </S.EntryItem>
+          </S.EntryList>
+          <S.TextWrapper>
+            <Text typograph={'bodyLargeMedium'} color={'Blue90'}>
+              질문의 답변과 결제는 팅장이 진행해요!
+            </Text>
+            <Text typograph={'bodyLargeMedium'} color={'Blue90'}>
+              팅원들은 팅장을 기다려주세요.
+            </Text>
+          </S.TextWrapper>
+        </S.EntryContainer>
+      </S.MainContainer>
 
       {props.context.isTeamLeader && (
         <S.ButtonWrapper>
