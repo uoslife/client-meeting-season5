c2c683f69e6a60eef8b5c838a5b9fd52a4d9c52e
jongbin26 <010jjj@naver.com>
2024-11-21 02:03:44 +0900

    feat: GroupSummaryPage 구현

--- src/pages/SummaryPage/SummaryPage.tsx
+++ src/pages/SummaryPage/SummaryPage.tsx
@@ -1,4 +1,61 @@
+import { useSearchParams } from 'react-router-dom';
+import Header from '../../components/common/Header';
+import Button from '../../components/common/Button';
+import Text from '../../components/common/Text';
+import S from './style';
+import useToast from '../../hooks/useToast';
+import GroupSummaryCard from '../../components/feature/GroupSummaryCard';
+
+const HEADER_TITLE = {
+  personal: '1대1 신청하기',
+  group: '3대3 신청하기',
+};
+
+const userInfo = {
+  name: '우채윤',
+  gender: '여성' as '여성' | '남성',
+  department: '디자인학과',
+  studentId: '17',
+  interest: ['운동', '게임', '카페', '맛집탐방'],
+  kakaoTalkId: 'woochy0827',
+};
+
 const SummaryPage = () => {
-  return <>SummaryPage</>;
+  const [searchParams] = useSearchParams();
+  const headerTitleType = searchParams.get('type') as 'personal' | 'group';
+  const toast = useToast();
+
+  return (
+    <S.Background>
+      <Header title={HEADER_TITLE[headerTitleType]} isGoBackButton={true} />
+      <S.Container className="layout-padding">
+        <S.MainContainer>
+          <Text
+            color={'Blue2'}
+            typograph={'headlineMedium'}
+            style={{ fontWeight: 700, width: '100%' }}
+          >
+            카드로 미리 보기
+          </Text>
+          <S.CardContainer>
+            <Text color="Blue2" typograph="titleMedium">
+              {`From. ${'팀이름'}`}
+            </Text>
+            <GroupSummaryCard toast={toast} userInfo={userInfo} />
+            <GroupSummaryCard toast={toast} userInfo={userInfo} />
+            <GroupSummaryCard toast={toast} userInfo={userInfo} />
+          </S.CardContainer>
+          <S.ToastWrapper>
+            {toast.render('카카오톡 ID가 복사되었습니다.')}
+          </S.ToastWrapper>
+        </S.MainContainer>
+      </S.Container>
+      <S.ButtonWrapper className="layout-padding">
+        <Button buttonColor="white" type="submit" onClick={() => {}}>
+          다음
+        </Button>
+      </S.ButtonWrapper>
+    </S.Background>
+  );
 };
 export default SummaryPage;
---
+++ src/pages/SummaryPage/style.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import summaryTree from '../../lib/assets/images/summary-tree.png';
+import summaryBackground from '../../lib/assets/images/summary-background.png';
+
+export const S = {
+  Background: styled.div`
+    background-image: url(${summaryBackground});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 100vw;
+    height: 100vh;
+    overflow: auto;
+  `,
+  ToastWrapper: styled.div`
+    position: relative;
+    // top:26px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  `,
+  Container: styled.div`
+    margin-top: 20px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  `,
+  MainContainer: styled.div`
+    width: 100%;
+    min-height: calc(100vh - 88px - 4rem);
+  `,
+  ButtonWrapper: styled.div`
+    background-image: url(${summaryTree});
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 120px;
+    margin-top: 27px;
+    padding-top: 35px;
+  `,
+  CardContainer: styled.div`
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  `,
+};
+
+export default S;
