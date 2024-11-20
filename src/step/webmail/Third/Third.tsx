b1ccab3465c1eace0c70ab47e3951362d6e49345
최동준 <choedongjun@choedongjun-ui-MacBookAir.local>
2024-11-21 05:15:47 +0900

    feat: webmail 결과 페이지 레이아웃 수정

--- src/step/webmail/Third/Third.tsx
+++ src/step/webmail/Third/Third.tsx
@@ -1,56 +1,44 @@
 import { ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Text from '../../../components/common/Text';
 import Button from '../../../components/common/Button';
-import tree from '../../../lib/assets/icon/tree.svg';
-import S from './style';
+
+import { S } from './style';
+import Text from '../../../components/common/Text';
+import WebmailImg from '../../../lib/assets/images/webmail-result-img.png';
 
 const Third = (): ReactNode => {
   const navigate = useNavigate();
   return (
-    <S.Wrapper>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 40,
-          alignItems: 'center',
-          transform: 'translateY(-25%)',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 8,
-          }}
-        >
-          <Text color={'Blue90'} typograph={'headlineMedium'}>
-            인증이 완료되었어요!
-          </Text>
-          <Text
-            color={'Blue70'}
-            typograph={'bodyMediumMedium'}
-            style={{ whiteSpace: 'pre-line', textAlign: 'center' }}
-          >
-            {`이제 내 정보를 입력하고 프로필을\n 만들 수 있어요`}
-          </Text>
-        </div>
-        <img src={tree} alt="tree" width={145} height={195} />
-      </div>
-
-      <div
-        className="layout-padding"
-        style={{ width: '100%', bottom: 36, position: 'absolute' }}
-      >
+    <S.Container className="layout-padding">
+      <S.MainContainer>
+        <S.Wrapper>
+          <S.IconWrapper>
+            <S.TextWrapper>
+              <Text typograph={'headlineMedium'} color="Blue90">
+                인증 완료! 환영합니다~
+              </Text>
+              <S.DescriptionWrapper>
+                <Text typograph={'bodyMediumMedium'} color="Blue70">
+                  이제 내 정보를 입력하고 프로필을
+                </Text>
+                <Text typograph={'bodyMediumMedium'} color="Blue70">
+                  만들 수 있어요.
+                </Text>
+              </S.DescriptionWrapper>
+            </S.TextWrapper>
+            <img src={WebmailImg} width="168" />
+          </S.IconWrapper>
+        </S.Wrapper>
+      </S.MainContainer>
+      <S.ButtonWrapper>
         <Button
           buttonColor={'primary'}
           onClick={() => navigate('/auth/profile')}
         >
           눈 맞을 짝을 찾아 떠나볼까요?
         </Button>
-      </div>
-    </S.Wrapper>
+      </S.ButtonWrapper>
+    </S.Container>
   );
 };
 
--- src/step/webmail/Third/style.ts
+++ src/step/webmail/Third/style.ts
@@ -1,12 +1,43 @@
 import styled from 'styled-components';
 
-const S = {
-  Wrapper: styled.div`
+export const S = {
+  Container: styled.div`
     width: 100%;
-    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  `,
+  MainContainer: styled.div`
+    width: 100%;
+    min-height: calc(100vh - 108px);
+    display: table;
+  `,
+  Wrapper: styled.div`
+    display: table-cell;
+    vertical-align: middle;
+  `,
+  IconWrapper: styled.div`
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
+  `,
+  TextWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+  `,
+  DescriptionWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
+  ButtonWrapper: styled.div`
+    width: 100%;
+    height: 88px;
+    margin-top: 20px;
   `,
 };
-export default S;
