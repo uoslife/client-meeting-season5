f677d94087dfbafadbf1ed5a8b35d30c06ddef71
최동준 <choedongjun@choedongjun-ui-MacBookAir.local>
2024-11-21 06:33:05 +0900

    style: 디자인 변경으로 인한 수정

--- src/step/BasicProfile/Fourth/Fourth.tsx
+++ src/step/BasicProfile/Fourth/Fourth.tsx
@@ -2,57 +2,42 @@ import { ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Text from '../../../components/common/Text';
 import Button from '../../../components/common/Button';
-import gingerman from '../../../lib/assets/images/gingerman.svg';
-import S from './style';
+import { S } from './style';
+import BasicImg from '../../../lib/assets/images/basic-result-img.png';
 
 const Fourth = (): ReactNode => {
   const navigate = useNavigate();
   return (
-    <S.Wrapper>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 40,
-          alignItems: 'center',
-          transform: 'translateY(-25%)',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            gap: 8,
-          }}
+    <S.Container className="layout-padding">
+      <S.MainContainer>
+        <S.Wrapper>
+          <S.IconWrapper>
+            <S.TextWrapper>
+              <Text typograph={'headlineMedium'} color="Blue90">
+                내 프로필 완성!
+              </Text>
+              <S.DescriptionWrapper>
+                <Text typograph={'bodyMediumMedium'} color="Blue70">
+                  미팅 신청에 필요한 프로필을 완성했어요.
+                </Text>
+                <Text typograph={'bodyMediumMedium'} color="Blue70">
+                  이제 원하는 짝을 찾으러 가볼까요 ?
+                </Text>
+              </S.DescriptionWrapper>
+            </S.TextWrapper>
+            <img src={BasicImg} width="180" />
+          </S.IconWrapper>
+        </S.Wrapper>
+      </S.MainContainer>
+      <S.ButtonWrapper>
+        <Button
+          buttonColor={'primary'}
+          onClick={() => navigate('/auth/profile')}
         >
-          <Text
-            color={'Blue90'}
-            typograph={'headlineMedium'}
-            style={{ fontWeight: 700 }}
-          >
-            내 프로필 완성!
-          </Text>
-          <Text
-            color={'Blue70'}
-            typograph={'bodyMediumMedium'}
-            style={{ whiteSpace: 'pre-line', textAlign: 'center' }}
-          >
-            {`미팅 신청에 필요한 프로필을 완성했어요.\n 이제 원하는 짝을 찾으러 가볼까요 ?`}
-          </Text>
-        </div>
-        <img src={gingerman} alt="tree" width={100} height={180} />
-      </div>
-
-      <div
-        className="layout-padding"
-        style={{ width: '100%', bottom: 36, position: 'absolute' }}
-      >
-        <Button buttonColor={'primary'} onClick={() => navigate('/auth/main')}>
-          팅 신청하기
+          눈 맞을 짝을 찾아 떠나볼까요?
         </Button>
-      </div>
-    </S.Wrapper>
+      </S.ButtonWrapper>
+    </S.Container>
   );
 };
 
