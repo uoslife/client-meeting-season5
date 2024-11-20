c11f1f1bf71d081f1ca60aace909c2eb19d4b6c3
jongbin26 <010jjj@naver.com>
2024-11-21 05:06:58 +0900

    feat: PaymentPage Success, Falied 구분 및 기존 페이지 제거

---
+++ src/pages/PaymentFailedPage/style.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const S = {
+  Wrapper: styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  `,
+  GradientText: styled.span`
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 32px;
+    letter-spacing: -0.6px;
+    background: var(
+      --Concept-Color,
+      linear-gradient(95deg, #4d6598 -0.68%, #ae1b2e 84.01%)
+    );
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  `,
+};
+export default S;
