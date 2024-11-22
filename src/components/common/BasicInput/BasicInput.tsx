52745ce0c3a9ecbbab23b1335b5ac459f98234de
최동준 <choedongjun@choedongjun-ui-MacBookAir.local>
2024-11-22 21:02:25 +0900

    feat: BasicInput 닫기 버튼 활성화

--- src/components/common/BasicInput/BasicInput.tsx
+++ src/components/common/BasicInput/BasicInput.tsx
@@ -1,5 +1,6 @@
-import { forwardRef } from 'react';
+import { forwardRef, useCallback, useRef, useState } from 'react';
 import { S } from './style';
+import close from '../../../lib/assets/icon/close.svg';
 
 interface BasicInputPropsType
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'defaultValue'> {
@@ -9,14 +10,51 @@ interface BasicInputPropsType
 
 const BasicInput = forwardRef<HTMLInputElement, BasicInputPropsType>(
   ({ type = 'text', defaultValue, ...props }, ref) => {
-    const safeDefaultValue = defaultValue === null ? undefined : defaultValue;
+    const [hasValue, setHasValue] = useState(!!defaultValue);
+    const inputRef = useRef<HTMLInputElement | null>(
+      null,
+    ) as React.MutableRefObject<HTMLInputElement | null>;
+
+    const inputRefCallback = useCallback(
+      (element: HTMLInputElement | null) => {
+        inputRef.current = element;
+        if (ref) {
+          if (typeof ref === 'function') {
+            ref(element);
+          } else if (ref && 'current' in ref) {
+            (ref as React.MutableRefObject<HTMLInputElement | null>).current =
+              element;
+          }
+        }
+      },
+      [ref],
+    );
+
+    const handleInput = (event: React.FormEvent<HTMLInputElement>) => {
+      const inputValue = (event.target as HTMLInputElement).value;
+      setHasValue(inputValue !== '');
+    };
+    const handleClear = () => {
+      if (inputRef && inputRef.current) {
+        inputRef.current.value = '';
+      }
+    };
+
     return (
-      <S.Input
-        type={type}
-        defaultValue={safeDefaultValue}
-        {...props}
-        ref={ref}
-      />
+      <S.Container>
+        <S.Input
+          type={type}
+          defaultValue={defaultValue || ''}
+          {...props}
+          ref={inputRefCallback}
+          onChange={handleInput}
+        />
+        {hasValue && (
+          <S.ClearButton onClick={handleClear} aria-label="Clear input">
+            <img src={close} alt="close" width={20} height={20} />
+          </S.ClearButton>
+        )}
+      </S.Container>
     );
   },
 );
--- src/components/common/BasicInput/style.ts
+++ src/components/common/BasicInput/style.ts
@@ -2,15 +2,21 @@ import styled from 'styled-components';
 import { COLORS } from '../../../lib/constants';
 
 export const S = {
+  Container: styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  `,
   Input: styled.input`
+    flex: 1;
     padding: 12px 0;
-
+    outline: none;
     border: 0;
     border-bottom: 1px solid ${COLORS.Blue20};
 
     width: 100%;
-    height: 100%;
-
     font-size: 1.6rem;
     font-weight: 500;
     line-height: 2.4rem;
@@ -18,6 +24,20 @@ export const S = {
     color: ${COLORS.Blue90};
     &::placeholder {
       color: ${COLORS.Blue20};
+    }
+  `,
+  ClearButton: styled.div`
+    position: absolute;
+    right: 0;
+    padding: 0 8px;
+    background: none;
+    border: none;
+    font-size: 1.4rem;
+    color: ${COLORS.Blue90};
+    cursor: pointer;
+
+    &:hover {
+      color: ${COLORS.Red50};
     }
   `,
 };
