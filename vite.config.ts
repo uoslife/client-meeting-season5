94cd74ab0811da318629d09fa41d9960b64c0c6c
jongbin26 <010jjj@naver.com>
2024-11-25 19:59:53 +0900

    feat: viteConfig에localhost ssl 인증 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
+import { ConfigEnv, loadEnv } from 'vite';
+import type { ServerOptions } from 'https';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const isDev = process.env.VITE_DEVELOP == 'true';
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      https: isDev
+        ? ({
+            key: fs.readFileSync('../../key/localhost-key.pem'),
+            cert: fs.readFileSync('../../key/localhost.pem'),
+          } as ServerOptions)
+        : undefined,
+      host: 'localhost',
+    },
+  });
+};
