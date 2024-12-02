4fed64420946ff84546e8aff1316727d408999f3
jongbin26 <010jjj@naver.com>
2024-12-02 16:00:25 +0900

    style: Tooltip z-index 수정

--- src/components/common/Tooltip/style.ts
+++ src/components/common/Tooltip/style.ts
@@ -18,7 +18,7 @@ export const S = {
     padding: 8px 12px;
     border-radius: 8px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    z-index: 1000;
+    z-index: 1;
     opacity: 1;
 
     transition:
